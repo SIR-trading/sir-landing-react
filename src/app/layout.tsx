1f8db433e481d32f6d38c22d2956122aad337e13
James Osunkiyesi <jaydhales@gmail.com>
2025-04-24 15:20:52 +0100

    feat: refactor app structure from Page to App router and implement new layout with TRPC and EVM provider

---
+++ src/app/layout.tsx
@@ -0,0 +1,56 @@
+import { Inter, Bebas_Neue } from "next/font/google";
+import "~/styles/globals.css";
+import { Header } from "~/components/layout/Header";
+import Bg from "../../public/background.png";
+import EvmProvider from "~/components/providers/evmProvider";
+import { headers } from "next/headers";
+import type { ReactNode } from "react";
+import { HydrateClient } from "~/trpc/server";
+import { TRPCProvider } from "~/trpc/client";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+const lora = Bebas_Neue({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-lora",
+});
+
+async function RootLayout({ children }: { children: ReactNode }) {
+  const cookie = (await headers()).get("cookie");
+  return (
+    <html lang="en">
+      <body
+        style={{
+          backgroundImage: `url(${Bg.src})`,
+          backgroundRepeat: "repeat",
+        }}
+        className={`relative ${lora.className} ${inter.className} text-white`}
+      >
+        <div
+          style={{
+            background:
+              "radial-gradient(55.25% 55.16% at 48.63% 44.84%, rgba(255, 255, 255, 0.15) 0%, rgba(255, 255, 255, 0.02) 100%)",
+            backgroundBlendMode: "lighten",
+            boxShadow: "0px 4px 0px 0px rgba(0,0,0,0.5)",
+          }}
+          className="absolute top-0 left-0 z-[-1] h-full w-full opacity-100"
+        ></div>
+        <TRPCProvider>
+          <HydrateClient>
+            <EvmProvider cookie={cookie}>
+              <div className="font-inter flex min-h-screen flex-col">
+                <Header />
+                <main className="content h-80">{children}</main>
+              </div>{" "}
+            </EvmProvider>
+          </HydrateClient>
+        </TRPCProvider>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
